2ff1ef01045815a61fe7b535be267ef7e7c9d9c5
LEIMIN123 <31990854+LEIMIN123@users.noreply.github.com>
2017-09-15 21:51:00 +0800

    Add files via upload

---
+++ Exercise01/Exercise01.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+print("the acronym of my name is:")
+print("L             MM               MM  Y           Y  NN        N")
+print("L             M M              MM    Y        Y   N N       N")
+print("L             M  M            M M      Y     Y    N  N      N")
+print("L             M   M          M  M       Y  Y      N   N     N")
+print("L             M    M        M   M        YY       N    N    N")
+print("L             M     M      M    M        YY       N     N   N")
+print("L             M      M    M     M        YY       N      N  N")
+print("L             M       M  M      M        YY       N       N N")
+print("LLLLLLLLLL    M        MM       M        YY       N        NN")
+print("The acronym of my name is: LMYN")
+
+
